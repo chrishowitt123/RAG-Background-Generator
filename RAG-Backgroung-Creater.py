cd245f474c0a553cbe077a15431ecd67b53ee9e8
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-15 18:14:44 +0100

    Update RAG-Backgroung-Creater.py

--- RAG-Backgroung-Creater.py
+++ RAG-Backgroung-Creater.py
@@ -1,20 +1,25 @@
 from PIL import Image
 
+# define dimensions 
 width_height_Red =  (120, 300) 
 width_height_Amber =  (30, 300) 
 width_height_Green =  (150, 300) 
+
 
 img1 = Image.new(mode = "RGB", size = width_height_Red, color = (252, 228, 214)) 
 img2 = Image.new(mode = "RGB", size = width_height_Amber, color = (255, 242, 204))
 img3 = Image.new(mode = "RGB", size = width_height_Green, color = (226, 239, 218))
 
 
+# join RAGs togeather
 def get_concat_h(img1, img2, img3):
-    dst = Image.new('RGB', (img1.width + img2.width + img3.width, img1.height))
-    dst.paste(img1, (0, 0))
-    dst.paste(img2, (img1.width, 0))
-    dst.paste(img3, (img1.width + img2.width, 0))
-    return dst
+    # define canvas size
+    cvs = Image.new('RGB', (img1.width + img2.width + img3.width, img1.height))
+    # paste images side-by-side
+    cvs.paste(img1, (0, 0))
+    cvs.paste(img2, (img1.width, 0))
+    cvs.paste(img3, (img1.width + img2.width, 0))
+    return cvs
 
 
 get_concat_h(img1, img2, img3)
