dae381f8ff0caf63594fa6ed0ef06f40356ef2aa
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-16 08:13:35 +0100

    Update RAG-Background-Generator.py

--- RAG-Background-Generator.py
+++ RAG-Background-Generator.py
@@ -16,8 +16,8 @@ img2 = Image.new(mode = "RGB", size = width_height_Amber, color = (255, 242, 204
 img3 = Image.new(mode = "RGB", size = width_height_Green, color = (226, 239, 218))
 
 
-# join RAGs togeather
-def get_concat_h(img1, img2, img3):
+# concatenate RAG images together horizontally
+def concat_images_h(img1, img2, img3):
     # define canvas size
     cvs = Image.new('RGB', (img1.width + img2.width + img3.width, img1.height))
     # paste images side-by-side
@@ -26,4 +26,4 @@ def get_concat_h(img1, img2, img3):
     cvs.paste(img3, (img1.width + img2.width, 0))
     return cvs
 
-get_concat_h(img1, img2, img3)
+concat_images_h(img1, img2, img3)
